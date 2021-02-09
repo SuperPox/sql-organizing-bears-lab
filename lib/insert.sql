504a6d5302c0807c24e3b900ce9716148c979174
SuperPox <calebkohl@gmail.com>
2021-02-08 22:04:51 -0700

    commit tree 53d0d4422a124aba168dc1b62f6b85f97e47baee

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (1, "MrChocolate", 1, "male", "black", "sweet", 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (2, "Rowdy", 1, "male", "brown", "active", 2);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (3, "Tabitha", 1, "female", "black", "sweet", 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (4, "Sergeant_Brown", 1, "male", "brown", "sweet", 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (5, "Melissa", 1, "female", "black", "sweet", 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (6, "Grinch", 8, "male", "green", "grouchy", 2);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (7, "Wendy", 2, "female", "white", "sweet", 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (8, NULL, 1, "male", "purple", "deadly", 2);
+
